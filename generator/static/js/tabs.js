321120df8d71ffd57a2c25bae67251f689a77220
anzeen <nazen93@yahoo.com>
2017-05-05 01:56:10 +0200

    fixed progress bar when clicking previous step, added button groups

--- generator/static/js/tabs.js
+++ generator/static/js/tabs.js
@@ -1,34 +1,47 @@
+var progress_bar = $('.progress-bar')[0];
+
 $('.next-step').click(function(){
   var next_tab = $('.nav-pills > .active').next('li');
-  var progress_bar = $('.progress-bar')[0];
   var progress_bar_class = $(progress_bar).attr('class');
   var progress_bar_value = $(progress_bar).attr('aria-valuenow');
   var progress_int_value = parseInt(progress_bar_value) + 33;
   
+  if (progress_int_value <= 100){
   $(progress_bar).attr('aria-valuenow', progress_int_value);
   $(progress_bar).html(progress_int_value + '% completed' );
   $(progress_bar).width(progress_int_value+'%');
   
-  new_bar(progress_bar, progress_bar_class);
+  new_bar(progress_bar, progress_bar_class, progress_int_value);
+  };
   
   $(next_tab).find('a').attr('data-toggle', 'tab');
   $(next_tab).find('a').trigger('click');
   $(next_tab).removeClass('disabled');
 });
 
-$('.btn-danger').click(function(){
-	  $('.nav-pills > .active').prev('li').find('a').trigger('click');
+$('.previous-step').click(function(){
+	var progress_bar_class = $(progress_bar).attr('class');
+	var progress_bar_value = $(progress_bar).attr('aria-valuenow');
+	var progress_int_value = parseInt(progress_bar_value) - 33;
+	
+	$(progress_bar).attr('aria-valuenow', progress_int_value);
+	$(progress_bar).html(progress_int_value + '% completed' );
+	$(progress_bar).width(progress_int_value+'%');
+	
+	new_bar(progress_bar, progress_bar_class, progress_int_value)
+	
+	$('.nav-pills > .active').prev('li').find('a').trigger('click');
 });
 
-function new_bar(object, bar_class){
+function new_bar(object, bar_class, value){
 	$(object).removeClass(bar_class);
-	if (bar_class.includes('danger')){
+	if (value == 33){
 		$(object).addClass('progress-bar progress-bar-warning progress-bar-striped active');
 	}
-	else if (bar_class.includes('warning')){
+	else if (value == 66){
 		$(object).addClass('progress-bar progress-bar-info progress-bar-striped active');
 	}
-	else if (bar_class.includes('info')){
+	else if (value == 99){
 		$(object).addClass('progress-bar progess-bar-success progress-bar-striped active');
 	}
 }
